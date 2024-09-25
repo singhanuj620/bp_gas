83a3af74b851fc660e5a4f3eaf2cd3ff8979add6
Anuj <singhanuj620@gmail.com>
2024-09-25 11:57:13 +0530

    feat(footer) : added footer

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,6 @@
+import BrowserLocation from "./BrowserLocation/BrowserLocation";
+import Homepage from "./Homepage/Homepage";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 
-export {
-  Navbar,
-  Footer,
-};
+export { BrowserLocation, Homepage, Navbar, Footer };
